9a656076cfddbcaecf770b08bbb325e2e66b90a1
IbrahimGomaa <ibrahim.gomaa1013@gmail.com>
2024-11-04 14:50:02 +0200

    finish authentication

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -12,6 +13,25 @@ const authConfig = {
   callbacks: {
     authorized({ auth, req }) {
       return !!auth?.user;
+    },
+    async signIn({ user }) {
+      try {
+        const currentUser = await getGuest(user?.email);
+        if (!currentUser) {
+          await createGuest({
+            email: user?.email,
+            fullName: user?.name,
+          });
+        }
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session?.user?.email);
+      session.user.guestId = guest?.id;
+      return session;
     },
   },
   pages: {
